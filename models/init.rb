3648e452980bfd30e4ff875bdf1b3c245b42d174
Jonathan Rey <jonatrey@yahoo.com>
2012-04-02 17:58:59 +0200

    Adding sqlite DB and updating README

--- models/init.rb
+++ models/init.rb
@@ -3,8 +3,10 @@ require 'dm-timestamps'
 require 'dm-validations'  
 require 'dm-migrations'
 
+#http://datamapper.org/docs/create_and_destroy.html
 DataMapper.setup :default, "sqlite://#{Dir.pwd}/database.db"  
 
+#Might be interesting for fixtures: https://github.com/notahat/machinist
 
 require "models/bill"
 require "models/user"
@@ -15,3 +17,4 @@ DataMapper.finalize
 
 # automatically create the Bill
 Bill.auto_upgrade!
+User.auto_upgrade!
