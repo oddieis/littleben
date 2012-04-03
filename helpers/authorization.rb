e8093aed8955aa9027c4734a3ba66081cab9aa79
Jonathan Rey <jonatrey@yahoo.com>
2012-04-03 19:42:58 +0200

    Documenting and cleaning the auth helper.

--- helpers/authorization.rb
+++ helpers/authorization.rb
@@ -1,33 +1,32 @@
 #encoding: utf-8
 module Authorization
-  def auth
-    @auth ||= Rack::Auth::Basic::Request.new(request.env)
-  end
- 
-  def unauthorized!(realm="littleben.com")
+  
+  #The current user can't access the current page.
+  def unauthorized!
     redirect "/login", 303
   end
  
+  #Is the current user logged in?
   def logged_in?
     session['logged_in']
     #false
   end
  
+  #Method called from the login controller to authenticate the user
   def authorize(username, password)
-    
+    #Basic logic to check name and password
     if(username == "jo" && password == "jo")
       session["logged_in"] = true
-      true
-    else
-      false
     end
-    
+    session["logged_in"]
   end
   
+  #Destroy the session
   def logout()
     session["logged_in"] = false
   end
  
+  #Called on every application's page
   def require_administrative_privileges
     unauthorized! unless logged_in? || (request.path_info == '/login' && !logged_in?)
   end
