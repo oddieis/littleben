47ab62a0d1ec504fc132d73bc7b8087ffab51249
Jonathan Rey <jonatrey@yahoo.com>
2012-04-05 12:30:01 +0200

    Changing models - removing content to avoid exceptions.

--- app/models/bill.rb
+++ app/models/bill.rb
@@ -1,11 +1,3 @@
 class Bill < ActiveRecord::Base
-    include DataMapper::Resource
-    property :id, Serial
-    property :title, String
-    property :summary, Text
-    property :body, Text
-    property :sponsor_id, Integer
-    property :vote_plus, Integer
-    property :vote_minus, Integer
-    property :created_at, DateTime
+
 end
