67941b60c2797fdd6061ac2efd332e0b04b80319
Jonathan Rey <jonatrey@yahoo.com>
2012-04-05 23:49:59 +0200

    Preparing logic and helpers around login.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,25 +1,14 @@
 class UsersController < ApplicationController
   # GET /users
   # GET /users.xml
-  def index
-    @users = User.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @users }
-    end
+  def index  
   end
 
   # GET /users/1
   # GET /users/1.xml
-  def show
-    @user = User.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @user }
-    end
-  end
+  def show  
+    @user = current_user  
+  end 
 
   # GET /users/new
   # GET /users/new.xml
@@ -80,4 +69,12 @@ class UsersController < ApplicationController
       format.xml  { head :ok }
     end
   end
+  
+  private  
+  def login_required  
+    unless current_user  
+      flash[:error] = 'You must be logged in to view this page.'  
+      redirect_to new_user_session_path  
+    end  
+  end  
 end
